e7c3a1f2a8ef118579fa331a6d2c4a7268551443
Gor Voskanyan <voskanyangor26@gmail.com>
2024-02-24 16:50:32 +0400

    add validator

--- mysite/shop/forms.py
+++ mysite/shop/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-
+from django.core import validators
 
 class ProductForm(forms.Form):
     name = forms.CharField(max_length=100)
     price = forms.DecimalField(min_value=1, max_value=100000)
-    description = forms.CharField(label='Product description', widget=forms.TextInput)
+    description = forms.CharField(
+        label='Product description',
+        widget=forms.TextInput,
+        validators=[validators.RegexValidator(
+            regex=r'great',
+            message='Field must contain word great'
+        )],
+    )
+
