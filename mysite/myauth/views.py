fc45840130900202db03d7dee64f558851a78e9b
Gor Voskanyan <voskanyangor26@gmail.com>
2024-03-05 14:14:46 +0400

    added cookies and sessions

--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
 
-def login_view(request: HttpRequest):
+def login_view(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
         if request.user.is_authenticated:
             return redirect('/admin/')
@@ -15,3 +15,24 @@ def login_view(request: HttpRequest):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         return render(request, 'myauth/login.html', {"error": "Invalid login credentials"})
+
+
+def set_cookie_view(request: HttpRequest) -> HttpResponse:
+    response = HttpResponse("Cookie set")
+    response.set_cookie("fizz", "buzz", max_age=3600)
+    return response
+
+
+def get_cookie_view(request: HttpRequest) -> HttpResponse:
+    value = request.COOKIES.get("fizz", "default value")
+    return HttpResponse(f"Cookie value: {value!r}")
+
+
+def set_session_view(request: HttpRequest) -> HttpResponse:
+    request.session["foobar"] = "spameggs"
+    return HttpResponse('Session set')
+
+
+def get_session_view(request: HttpRequest) -> HttpResponse:
+    value = request.session.get('foobar', 'default value')
+    return HttpResponse(f"Session value {value!r}")
