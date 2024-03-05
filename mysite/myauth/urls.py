28fc98362f09b7e1304eaee61670f55f267fcad0
Gor Voskanyan <voskanyangor26@gmail.com>
2024-03-05 15:22:43 +0400

    added logout

--- mysite/myauth/urls.py
+++ mysite/myauth/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 
 from .views import (
     login_view,
+    logout_view,
     get_cookie_view,
     set_cookie_view,
     set_session_view,
     get_session_view,
+    MyLogoutView,
 )
 
 appname = 'myauth'
@@ -19,8 +21,12 @@ urlpatterns = [
              redirect_authenticated_user=True
          ),
          name='login'),
+
+    path('logout/', MyLogoutView.as_view(), name='logout'),
+
     path('cookie/get/', get_cookie_view, name='cookie-get'),
     path('cookie/set/', set_cookie_view, name='cookie-set'),
+
     path('session/set/', set_session_view, name='session-set'),
     path('session/get/', get_session_view, name='session-get'),
 ]
