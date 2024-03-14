baaac0aa446190ebdb8d7c866bf705506b14c526
Gor Voskanyan <voskanyangor26@gmail.com>
2024-03-14 14:46:35 +0400

    added multiple files upload but has bugs

--- mysite/shop/forms.py
+++ mysite/shop/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.forms.utils import flatatt
+from django.utils.safestring import mark_safe
+
 from django.contrib.auth.models import Group
 from django.core import validators
 
@@ -17,10 +20,10 @@ from .models import Product
 #         )],
 #     )
 
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = "name", "price", "description", "discount"
+# class ProductForm(forms.ModelForm):
+#     class Meta:
+#         model = Product
+#         fields = "name", "price", "description", "discount"
 
 
 class GroupForm(forms.ModelForm):
@@ -28,11 +31,26 @@ class GroupForm(forms.ModelForm):
         model = Group
         fields = ["name"]
 
+
+class MultipleClearableFileInput(forms.ClearableFileInput):
+    def render(self, name, value, attrs=None, renderer=None):
+        if value is None:
+            value = []
+
+        final_attrs = self.build_attrs(attrs, {'type': 'file', 'name': name, 'multiple': 'multiple'})
+        input_html = '<input{} />'.format(flatatt(final_attrs))
+
+        return mark_safe(input_html)
+
+    def value_from_datadict(self, data, files, name):
+        if isinstance(files.get(name), list):
+            return files.getlist(name)
+        return files.get(name, None)
+
+
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = "name", "price", "description", "discount", "preview"
+        fields = ["name", "price", "description", "discount", "preview", "images"]
 
-    images = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={"mulitple": True})
-    )
+    images = forms.ImageField(widget=MultipleClearableFileInput)
