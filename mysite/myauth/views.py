9c6eb500c1582583ea4f1f68b099c3f2b18b4cfc
Gor Voskanyan <voskanyangor26@gmail.com>
2024-03-14 14:59:27 +0400

    added simple view

--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -11,6 +11,11 @@ from django.views.generic import TemplateView, CreateView
 from .models import Profile
 
 
+class HelloView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return HttpResponse('<h1>Hello world!</h1>')
+
+
 class AboutMeView(TemplateView):
     template_name = "myauth/about-me.html"
 
