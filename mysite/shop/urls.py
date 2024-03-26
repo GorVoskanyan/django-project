809c772d5992848ea9d7dda253a89cba648a6c44
Gor Voskanyan <voskanyangor26@gmail.com>
2024-03-26 21:19:56 +0400

    added routers for api urls

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
 from .views import (
     ShopIndexView,
     GroupsListView,
@@ -10,12 +13,17 @@ from .views import (
     ProductUpdateView,
     ProductDeleteView,
     ProductsDataExportView,
+    ProductViewSet,
 )
 
 app_name = 'shop'
 
+routers = DefaultRouter()
+routers.register("products", ProductViewSet)
+
 urlpatterns = [
     path('', ShopIndexView.as_view(), name='index'),
+    path('api/', include(routers.urls)),
     path('groups/', GroupsListView.as_view(), name='groups'),
     path('products/', ProductsListView.as_view(), name='products'),
     path('products/<int:pk>/', ProductDetailsView.as_view(), name='product_details'),
