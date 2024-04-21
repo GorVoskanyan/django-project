7de4e876c785ecf352879fb0174440998b255870
Gor Voskanyan <voskanyangor26@gmail.com>
2024-04-21 19:49:21 +0400

    added transaction

--- mysite/shop/management/commands/create_order.py
+++ mysite/shop/management/commands/create_order.py
@@ -1,18 +1,25 @@
+from typing import Sequence
+
 from django.core.management import BaseCommand
 from django.contrib.auth.models import User
+from django.db import transaction
 
-from shop.models import Order
+from shop.models import Order, Product
 
 class Command(BaseCommand):
+    @transaction.atomic
     def handle(self, *args, **options):
-        self.stdout.write('Create order')
+        self.stdout.write('Create order with products.')
         user = User.objects.get(username='admin')
-
+        products: Sequence[Product] = Product.objects.all()
         order, created = Order.objects.get_or_create(
-            delivery_address='Avenue Komitas 15',
+            delivery_address='Avenue Arshakunyats 15',
             promocode='promo888',
             user=user,
-
         )
 
+        for product in products:
+            order.products.add(product)
+        order.save()
+        order + 1
         self.stdout.write(f'Created order {order}')
